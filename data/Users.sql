8d6f7e0f570ac0f56d0992d4ddacfeaaf8e0e0ed
popoul0s <evan.louispro@gmail.com>
2023-09-22 11:19:10 +0200

    UsersTrait

--- data/Users.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE 'Users' (
-'id' INTEGER DEFAULT NULL PRIMARY KEY AUTOINCREMENT,
-'username' TEXT UNIQUE NOT NULL ,
-'password' TEXT NOT NULL 
-);
-
-
-INSERT INTO Users('username','password') VALUES('admin','admin');
